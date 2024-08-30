5ef00a0dd60f2636216bce937c63912206773a81
vandilsonbrito <vandilson25@outlook.com>
2024-08-30 17:47:02 -0300

    Feat: create blog pages interfaces

---
+++ utils/interafces.ts
@@ -0,0 +1,13 @@
+export interface simpleBlogCard {
+    title: string,
+    smallDescription: string,
+    currentSlug: string,
+    titleImage: any
+}
+
+export interface fullBlogData {
+    currentSlug: string,
+    title: string,
+    content: any,
+    titleImage: any
+}
