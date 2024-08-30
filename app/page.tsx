810bde285fd3a7462e5257c366462378045cc7ed
vandilsonbrito <vandilson25@outlook.com>
2024-08-30 17:36:14 -0300

    Feat: create getData function to fetch data from sanity studio

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,17 @@ import Image from "next/image";
 
 export const revalidate = 30;
 
+async function getData() {
+  const query = `
+  *[_type == 'blog'] {
+    title,
+      smallDescription,
+      "currentSlug": slug.current,
+      titleImage
+  }`;
+  const data = await client.fetch(query);
+  return data;
+};
 
 
 
